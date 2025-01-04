8cf123121853fe3bd1473780581aa2c4d4be0dfe
Manikandan <manimk1704@gmail.com>
2025-01-04 22:11:42 +0530

    Basic Template structure lecture

---
+++ Practice Basic Template Structure/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Header } from "./components/Header";
+import { Footer } from './components/Footer';
+
+export default function App() {
+
+  return (
+    <>
+    <Header />
+    <div>
+    <h1>Mani</h1>
+    </div>
+    <Footer />
+    </>
+  );
+}
---
+++ Practice Basic Template Structure/src/components/Header.js
@@ -0,0 +1,9 @@
+import Logo from "../assets/logo.png";
+import "./header.css";
+
+export const Header = () => {
+
+    return(
+        <img className = "logo" src = {Logo} alt = "" />
+    );
+}
