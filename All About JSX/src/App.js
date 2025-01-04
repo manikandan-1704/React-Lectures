aa9e70016cf0f5a5715a3774aaf3365c188f83b8
Manikandan <manimk1704@gmail.com>
2025-01-04 13:11:02 +0530

    JSX Lecture Files

---
+++ All About JSX/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+export default function App() {
+
+  const username = "Mani";
+
+  return (
+    <>
+      <h1 className= 'active'>{username}</h1>
+      <p>This is Demo</p>
+    </>
+  );
+}
+
+
