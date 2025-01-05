dc27fa8b995d0a27541d45fa745a0f35cdcf02a8
Manikandan <manimk1704@gmail.com>
2025-01-05 22:39:34 +0530

    All about conditional templates lecture

---
+++ conditional templates/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  const [tasks, setTasks] = useState([
+    { id: 1, name: 'React Lectures', completed: false },
+    { id: 2, name: 'useState Lectures', completed: true },
+    { id: 3, name: 'States Lectures', completed: false },
+  ]
+  );
+  const [show, setShow] = useState(true);
+  function handleDelete(id){
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+  return (
+    <div className='App'>
+      <h1>Task List</h1>
+      <ul>
+        <button className = 'trigger' onClick = {() => setShow(!show)}>Toggle</button>
+        {show && tasks.map((task) => (
+          <li key={task.id} className={task.completed ? 'completed' : 'incomplete'}>
+            <span>{task.id} - {task.name}</span>
+            <button onClick = {()=> handleDelete(task.id)} className = 'delete'>Delete</button>
+          </li>
+        ))}
+        
+      </ul>
+    </div>
+  );
+}
+
+export default App;
