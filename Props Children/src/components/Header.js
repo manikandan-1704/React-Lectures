e3e1924a8dce2aee951f7652272d0ca0fe67d0d1
Manikandan <manimk1704@gmail.com>
2025-01-06 19:08:49 +0530

    props children lecture

---
+++ Props Children/src/components/Header.js
@@ -0,0 +1,10 @@
+import Logo from "../assets/lin.png"
+
+export const Header = () => {
+  return (
+    <header>
+        <img src={Logo} alt="" />
+        <a href="/">Home</a>
+    </header>
+  )
+}
