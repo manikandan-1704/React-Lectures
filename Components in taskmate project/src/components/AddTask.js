a250c20803268a7cd50a14c06d4ee8d492d83b2c
Manikandan <manimk1704@gmail.com>
2025-01-09 14:58:11 +0530

    taskmate project [components] -1

---
+++ Components in taskmate project/src/components/AddTask.js
@@ -0,0 +1,10 @@
+export const AddTask = () => {
+  return (
+    <section className="addTask">
+      <form>
+        <input type = "text" name = "task" placeholder="Add Task" autoComplete="off" />
+        <button type = "submit">Add</button>
+      </form>
+    </section>
+  )
+}
