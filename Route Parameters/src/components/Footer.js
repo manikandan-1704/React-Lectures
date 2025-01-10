3528b487ffc3778393bab718c63964b017de94e9
Manikandan <manimk1704@gmail.com>
2025-01-10 12:05:41 +0530

    route parameters lecture

---
+++ Route Parameters/src/components/Footer.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+export const Footer = () => {
+    return (
+        <footer>
+            <Link to="/">2030 Routemate</Link>
+        </footer>
+    )
+}
---
+++ Route Parameters/src/components/ProductDetail.js
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+
+export const ProductDetail = () => {
+
+    const params = useParams();
+
+    return(
+        <div className="component">ProductDetail - {params.id}</div>
+    )
+}
