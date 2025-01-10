2213e896910e85c6cc797a23af58e5a537d561e0
Manikandan <manimk1704@gmail.com>
2025-01-10 12:51:39 +0530

    nested routes lecture

---
+++ Nested Routes/src/App.js
@@ -0,0 +1,40 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import './App.css';
+import { Home } from "./components/Home";
+import { ProductList } from "./components/ProductList";
+import { ProductDetail } from "./components/ProductDetail";
+import { Contact } from "./components/Contact";
+import { Admin } from "./components/Admin";
+import { PageNotFound } from "./components/PageNotFound";
+import { ContactIn } from "./components/ContactIn";
+import { ContactUs } from "./components/ContactUs";
+import { ContactEu } from "./components/ContactEu";
+
+function App() {
+  const user = false;
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="products/:id" element={<ProductDetail />} />
+          <Route path="contact" element={<Contact />} >
+            <Route path="in" element={<ContactIn />} />
+            <Route path="us" element={<ContactUs />} />
+            <Route path="eu" element={< ContactEu />} />
+          </Route>
+          <Route path="admin" element={user ? <Admin /> : <Navigate to="/" />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Nested Routes/src/components/Contact.js
@@ -0,0 +1,18 @@
+import { useNavigate, Outlet } from "react-router-dom";
+
+export const Contact = () => {
+
+    const navigate = useNavigate();
+    const handleSubmit = () => {
+        console.log("------");
+        return navigate("/");
+    }
+
+    return (
+        <>
+            <div className="component">Contact</div>
+            <button onClick={handleSubmit}>Submit Form</button>
+            <Outlet />
+        </>
+    )
+}
