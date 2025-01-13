30ad98008f77be52d3bb74e29769e2ff9cfc8ea4
Manikandan <manimk1704@gmail.com>
2025-01-13 14:28:08 +0530

    cinemate project - movie list structure - 06

---
+++ Movie List Structure - Cinemate - 06/src/pages/MovieList.js
@@ -0,0 +1,15 @@
+import {Card} from '../components';
+
+export const MovieList = () => {
+  return (
+    <main>
+      <div>
+        <section className="max-w-7xl mx-auto py-7">
+          <div className="flex justify-start flex-wrap">
+            <Card />
+          </div>
+        </section>
+      </div>
+    </main>
+  )
+}
