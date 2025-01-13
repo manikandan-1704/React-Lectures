892ceea294765e48e0729eef7955da21be700949
Manikandan <manimk1704@gmail.com>
2025-01-13 15:20:15 +0530

    cinemate project - customHook useFetch - 08

---
+++ customHook useFetch - cinemate - 08/src/pages/MovieList.js
@@ -0,0 +1,18 @@
+import {Card} from '../components';
+import {useFetch} from '../hooks/useFetch';
+export const MovieList = ({apiPath}) => {
+  const {data: movies} = useFetch(apiPath)
+  return (
+    <main>
+      <div>
+        <section className="max-w-7xl mx-auto py-7">
+          <div className="flex justify-start flex-wrap">
+            {movies.map((movie) =>(
+              <Card key={movie.id} movie={movie} />
+            ))}
+          </div>
+        </section>
+      </div>
+    </main>
+  )
+}
